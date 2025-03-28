bd94c59da5681ed26d0236da8a7f6cae09263f28
sharick2408 <garciacruzwilliam2@gmail.com>
2025-03-28 12:53:21 -0500

    Java_array 1

---
+++ Java 1 Sharick.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.primera_funcion;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Primera_funcion {
+
+    public static void main(String[] args) {
+        int resultado;
+        resultado = suma (50,50000000);
+        System.out.println(resultado);
+                
+                
+    
+        
+    }
+    public static int  suma(int a,int b){
+    return a+b;
+}
+}
